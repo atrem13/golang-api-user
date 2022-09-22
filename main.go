95639dc5f6f268575e40118d533a83ae883b6ead
atrem13 <mertayoga17.my@gmail.com>
2022-09-22 15:11:52 +0800

    update

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func setupRouter() *gin.Engine {
 	r.POST("/users", userRepo.CreateUser)
 	r.GET("/users", userRepo.GetUsers)
 	r.GET("/users/:id", userRepo.GetUser)
-	r.PUT("/users/:id", userRepo.UpdateUser)
+	r.PATCH("/users/:id", userRepo.UpdateUser)
 	r.DELETE("/users/:id", userRepo.DeleteUser)
 
 	return r
